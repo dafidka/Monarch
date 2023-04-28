1b879b2916ded9129265ca6c412d05a8452a49ec
Dafid <Dafid@DAFPAD>
2023-04-28 18:12:25 +0200

    Add project files.

---
+++ Editor/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Editor.GameProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;   //wait for main screen to load
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenStartupScreen();    //then call the startup screen
+        }
+
+        private void OpenStartupScreen()
+        {
+            var startupScreen = new StartupScreen();
+            startupScreen.ShowDialog(); //only the startup screen is interactable
+            if (startupScreen.ShowDialog() == false)    //if dialog doesn't exist   
+            {
+                Application.Current.Shutdown();         //close main window too
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
